019ad8bf7ff6ba5fd715be2d01cc58bcf2640166
hssnyounus <hssnyounus@gmail.com>
2024-08-30 17:23:20 +0500

    get all data from type model

--- backend/api/controllers/typeController .js
+++ backend/api/controllers/typeController .js
@@ -1,23 +1,32 @@
 import models from '../models';
 // import { ProductRepo } from '../repositories/ProductRepo';
-const {type} = models;
+const { type } = models;
 // const {Category} = models;
 const types = {
-    async create(req,res,next){
+    async create(req, res, next) {
         try {
 
-            const {name} = req.body;
+            const { name } = req.body;
             console.log(req.body);
             const typeCreate = await type.create({
                 name
             });
-          
-            return res.status(201).send({typeCreate,message:{success:'type created'}})
+
+            return res.status(201).send({ typeCreate, message: { success: 'type created' } })
         } catch (error) {
             return next(new Error(error))
         }
     },
 
+    async getAll() {
+        try {
+            const data = await type.findAll();
+
+            return res.status(200).send({ data, message: { success: "type all data" } })
+        } catch (error) {
+
+        }
+    }
 
 }
 
